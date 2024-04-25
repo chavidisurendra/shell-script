186d192484fd03f11a697535646a915d5b250b7d
chavidisurendra <chavidisurendra@gmail.com>
2024-04-25 12:50:50 +0530

    on

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NUMBER=$1
-
+num=$2
 if [ $NUMBER -gt 10 ]
 then
     echo "Given number $NUMBER is greater than 10"
@@ -10,3 +10,11 @@ else
 fi
 
 #-gt, -lt, -eq, -ge, -le
+
+if [ $num -eq 5]
+then
+    echo "given numbe $num is equal to 5"
+else
+   echo "given number $num is nt equal to 5"
+
+fi
