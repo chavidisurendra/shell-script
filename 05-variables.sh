d8f3e12b656d0c297b0a1907cd5266689b0d7775
chavidisurendra <chavidisurendra@gmail.com>
2024-04-25 12:31:51 +0530

    some msf

--- 05-variables.sh
+++ 05-variables.sh
@@ -9,3 +9,10 @@ echo "Please enter password::"
 read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+echo "enter name :"
+read NAME
+echo "enter password"
+read -s PASSWORD
+
+echo "name is: $NAME, Password is: $PASSWORD"
