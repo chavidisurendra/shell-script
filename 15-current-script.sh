4df6d9e315b29db20d401ea940e4857580748ab0
chavidisurendra <chavidisurendra@gmail.com>
2024-04-28 11:05:48 +0530

    the scipt

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 COURSE="DevOps from Current Script"
-
+model="hunter bike"
 echo "Before calling other script, course: $COURSE"
+echo "Before calling other script, model: $model"
 echo "Process ID of current shell script: $$"
 
 #./16-other-script.sh
@@ -10,3 +11,4 @@ echo "Process ID of current shell script: $$"
 source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
+echo "After calling other script, model: $model"
--- 16-other-script.sh
+++ 16-other-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COURSE="DevOps from Other Script"
-
+model="pulsar"
 echo "Variable value from Other script: $COURSE"
+echo "Variable value from other script: $model"
 echo "Process ID of other script: $$"
